0753f5ca8ed2f8d15aab4dde2e7dda2665959068
gafelix10@gmail.com <gafelix10@gmail.com>
2020-01-15 22:09:21 -0300

    Implementando circuit breaker com resilience4j

--- src/main/java/com/green/cashback/greenCashback/repository/CashbackRepository.java
+++ src/main/java/com/green/cashback/greenCashback/repository/CashbackRepository.java
@@ -3,10 +3,13 @@ package com.green.cashback.greenCashback.repository;
 import com.green.cashback.greenCashback.client.CashBackHttpClient;
 import com.green.cashback.greenCashback.model.Bottle;
 import com.green.cashback.greenCashback.model.CashBack;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 /**
  * @author gfaraujo
@@ -17,7 +20,12 @@ public class CashbackRepository {
     @Autowired
     private CashBackHttpClient client;
 
+    @CircuitBreaker(name = "cacheBackCalculate", fallbackMethod = "fallbackCalculate")
     public CashBack obtainCashbackCalculated(final int bottlesQuantity) throws IOException, InterruptedException {
         return client.obtainCashbackCalculated(new Bottle(bottlesQuantity));
+    }
+
+    private CashBack fallbackCalculate(final int bottlesQuantity,HttpServerErrorException ex) {
+        return new CashBack(BigDecimal.ZERO);
     }
 }
